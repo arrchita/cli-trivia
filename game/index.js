6507bdb8b913cc5b1a74eb5e6aeb65857637726b
archita <deepg9997@gmail.com>
2024-10-03 00:01:15 +0530

    edited index.js

--- game/index.js
+++ game/index.js
@@ -13,6 +13,20 @@ class Question{
 }
 
 async function main() {
+
+    const question1 = new Question(
+		"1) In what year was PowerShell, a command-line shell and scripting language developed by Microsoft, first released?",
+		[
+		"1993",
+		"1999",
+		"2006",
+		"2014"],
+		2,
+		"s",
+		"a",
+	)
+
+
     p.intro(`${color.bgMagenta(color.black('Welcome to the trivia!!'))}`);
     p.outro(`${color.bgMagenta(color.black('Thanks for taking the quiz!'))}`);
 }
